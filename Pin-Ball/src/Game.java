3722a1ec27e30ad4c7b2ce8a793c82d09ed72cfa
cgrubbs312125 <cgrubbs312125@gmail.com>
2018-06-05 11:23:12 -0400

    gamestuff

--- Pin-Ball/src/Game.java
+++ Pin-Ball/src/Game.java
@@ -1,4 +1,46 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class Game {
+public class Game extends Thread
+{
+	
+	public void init()
+	{
+		JFrame f = new JFrame("Pinball VS");
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Container c = f.getContentPane();
+		c.setBackground(Color.GRAY);
+		Dimension d = new Dimension(200,500);
+		c.setPreferredSize(d);
+		f.setResizable(false);
+		f.setVisible(true);
+	}
+	
+	public void start()
+	{
+	
+	}
+	
+	public void run()
+	{
+		
+	}
+	
+	public void paint()
+	{
+	
+	}
+	
+	public void update()
+	{
+		
+	}
+	
+	}
 
+	public void end()
+	{
+		
+	}
+	
 }
