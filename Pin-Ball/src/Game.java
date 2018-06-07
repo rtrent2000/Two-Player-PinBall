f4fae696ac72ef96e38e2224554f571b26645fd1
cgrubbs312125 <cgrubbs312125@gmail.com>
2018-06-07 07:10:38 -0700

    more stufff

--- Pin-Ball/src/Game.java
+++ Pin-Ball/src/Game.java
@@ -1,9 +1,29 @@
 import java.awt.*;
 import javax.swing.*;
+import java.util.Timer;
 
-public class Game extends Thread
+public class Game
 {
 	
+	private int secondsPassed = 0;
+	private Timer timer = new Timer(); // game timer that pretty much controls E V E R Y T H I N G
+	
+	private TimerTask fps = new TimerTask() // basic game controls which alters fps and checks user input
+		public void run() {
+			repaint();
+			//check user input
+		}
+	
+	private TimerTask secondCounter = new TimerTask() // counts seconds passed in game
+		public void run() {
+			secondsPassed ++;
+		}
+	
+	private TimerTask reset = new TimerTask() // resets game at round end (2:00) and also checks for winner
+		public void run() {
+			
+		}
+		
 	public void init()
 	{
 		JFrame f = new JFrame("Pinball VS");
@@ -18,13 +38,11 @@ public class Game extends Thread
 	
 	public void start()
 	{
-	
+		timer.scheduleAtFixedRate(fps,17);
+		timer.scheduleAtFixedRate(secondCounter,1000);
+		timer.scheduleAtFixedRate(reset,);
 	}
 	
-	public void run()
-	{
-		
-	}
 	
 	public void paint()
 	{
