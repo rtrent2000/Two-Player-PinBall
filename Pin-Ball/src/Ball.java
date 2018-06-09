3a1db87da3b615bf93b6639ad0fd82c6c6abd5dc
cgrubbs312125 <cgrubbs312125@gmail.com>
2018-06-08 19:01:34 -0700

    goeee

--- Pin-Ball/src/Ball.java
+++ Pin-Ball/src/Ball.java
@@ -1,4 +1,88 @@
 
 public class Ball {
 
+	
+	private int x, y, dx, dy;
+	private final int radius =   , grav =  	   ;
+	private graphicsAskkClaywhatthisis frame;
+	
+	public Ball(graphicsAskkClaywhatthisis g)
+	{
+		x = g.width()/2;
+		y = g.length()/2;
+		dx = 
+		dy = 
+		
+	}
+		
+	public int getX()
+	{
+		return x;
+	}
+	
+	public int getY()
+	{			
+		return y;
+	}
+	
+	
+	
+	public void move()
+	{
+		
+		if(						//if the ball passes middle
+		{
+			
+			
+			
+		}
+			
+			
+		if(x+ dx > g.frame.getWidth()- radius)    //if the ball hits the right side of the board
+		{
+			x =frame.getWidth() - radius -1;
+			dx =-dx;	
+		}
+		
+		else if(x+dx < 0)
+		}			
+			x = 0 + radius;
+			dx = -dx
+		//f(x < frame.getWidth()/2 )
+				
+			
+			
+		}
+	
+	
+		if()
+		{
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- Pin-Ball/src/Game.java
+++ Pin-Ball/src/Game.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Graphics;
 import java.awt.event.*;
+import java.awt.Color;
 import java.util.*;
 import java.util.Timer;
 
-public class Game
+public class Game extends JApplet
 {
 		private static int secondsPassed = 0, p1Wins = 0, p2Wins = 0, p1Score = 0, p2Score = 0, pot = 0;
 	    private static Timer timer = new Timer(); // game timer that pretty much controls E V E R Y T H I N G
@@ -27,7 +29,6 @@ public class Game
 	    { // resets game at round end (2:00) and also checks for winner and adds to player score
 	        public void run() 
 	        {
-	            
 	            if (p1Score > p2Score)
 	                p1Wins++;
 	            else if (p2Score > p1Score)
@@ -44,7 +45,7 @@ public class Game
 	        startup.setSize(500,700);
 	        startup.setResizable(false);
 	        startup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        startup.setVisible(true);
+	        
 	        
 	        JButton start = new JButton("Start!");
 	        start.addActionListener(new ActionListener()
@@ -54,39 +55,39 @@ public class Game
 	                start();
 	            }
 	        });
-	        JLabel label = new JLabel("PINBALL VS");
+	        
 	        JPanel panel = new JPanel();
 	        startup.add(panel);
+	        JLabel label = new JLabel("PINBALL VS");
+	        panel.setBackground(Color.BLACK);
 	        panel.add(label);
 	        panel.add(start);
+	        startup.setVisible(true);
+	        
 	        // lets put highscores here
 	    }
 	    
-	    private static void start()
+	    public void start()
 	    {
 	        timer.scheduleAtFixedRate(fps,(long)17,(long)17);
 	        timer.scheduleAtFixedRate(secondCounter,(long)1000,(long)1000);
 	        timer.scheduleAtFixedRate(reset,120000,120000);
 	        JFrame game = new JFrame("Pinball VS");
+	        JPanel panel = new JPanel();
 	        game.setSize(1366,768);
 	        game.setResizable(false);
 	        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        game.setVisible(true);
-	        paint();
 	    }
 	    
 	    
-	    private static void paint()
+	    public void paint(Graphics g)
 	    {
+	    	super.paint(g);
+	    	g.fillOval(x-radius,y-radius,radius*2,radius*2);
 	    }
 	    
-	    private static void repaint()
-	    {
-	        update();
-	        paint();
-	    }
-	    
-	    private static void update()
+	    public void update()
 	    {
 	        
 	    }
